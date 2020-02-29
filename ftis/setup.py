cd16f5e9dfea816cfa3329bd07316d6ff8c32526
James Bradbury <jamesbradbury93@gmail.com>
2020-02-29 22:42:37 +0000

    update deps

--- ftis/setup.py
+++ ftis/setup.py
@@ -1,23 +1,23 @@
 import setuptools
 
 setuptools.setup(
-    name='ftis',
-    version='1.0',
-    author='James Bradbury',
-    author_email='jamesbradbury93@gmail.com',
-    description='A package containing recipes for finding things in stuff.',
-    packages=['ftis'],
+    name="ftis",
+    version="1.0",
+    author="James Bradbury",
+    author_email="jamesbradbury93@gmail.com",
+    description="The finding things in stuff package.",
+    packages=["ftis"],
     install_requires=[
-        'PyYAML',
-        'python-rapidjson',
-        'SoundFile',
-        'simpleaudio',
-        'sklearn',
-        'hdbscan',
-        'numpy',
-        'scikit-learn',
-        'scipy',
-        'umap-learn',
-        'pydub'
-    ]
+        "PyYAML",
+        "python-rapidjson",
+        "SoundFile",
+        "simpleaudio",
+        "sklearn",
+        "hdbscan",
+        "numpy",
+        "scikit-learn",
+        "scipy",
+        "umap-learn",
+        "pydub",
+    ],
 )
