ecb21e416ae385b328e19e24c1653e465e39a9e9
James Bradbury <jamesbradbury93@gmail.com>
2020-02-29 22:41:59 +0000

    NotYetImplemented exception

--- ftis/ftis/common/exceptions.py
+++ ftis/ftis/common/exceptions.py
@@ -2,10 +2,17 @@ class InvalidYamlError(Exception):
     def __init__(self, msg: str):
         super().__init__(msg)
 
+
 class AnalyserNotFound(Exception):
     def __init__(self, msg: str):
         super().__init__(msg)
 
+
 class AnalyserParameterInvalid(Exception):
     def __init__(self, msg: str):
         super().__init__(msg)
+
+
+class NotYetImplemented(Exception):
+    def __init__(self):
+        super().__init__("This function is not implemented yet")
