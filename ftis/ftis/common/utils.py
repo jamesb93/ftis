6df0c166333da8b61e7ebeada4b4754bd5704e3b
James Bradbury <jamesbradbury93@gmail.com>
2020-02-29 22:41:40 +0000

    scrub metadata and expand home folder

--- ftis/ftis/common/utils.py
+++ ftis/ftis/common/utils.py
@@ -1,31 +1,52 @@
-import os, yaml, csv
+import os
+import yaml
 import soundfile as sf
 import simpleaudio as sa
 from importlib import import_module
+
 # JSON importing
 try:
     import rapidjson as rj
 except ImportError:
     import json as rj
 
+
+def expand_tilde(path: str) -> str:
+    if path[0] == "~":
+        return os.path.expanduser(path)
+    else:
+        return path
+
+
+def scrub_metadata(dict_to_scrub: dict) -> dict:
+    # FIXME: optimise without temporary container
+    temp_dict = dict_to_scrub
+    if "meta" in temp_dict:
+        del temp_dict["meta"]
+    return temp_dict
+
+
 def import_analyser(class_name: str):
     path = f"ftis.analysers.{class_name}"
     module = import_module(path)
     return module.main
 
+
 def lines_to_list(input_file: str) -> list:
     """Take the lines of a file and return it as a list"""
-    with open(input_file, 'r') as f:
+    with open(input_file, "r") as f:
         lines = f.readlines(0)
         content = [x.strip() for x in lines]
         return content
+
 
 def check_make(dir_path: str):
     """Create a directory if it doesn't exist"""
     try:
         os.mkdir(dir_path)
     except FileExistsError:
-        print(f'Directory {dir_path} already exists.')
+        print(f"Directory {dir_path} already exists.")
+
 
 def cd_up(path: str, num: int) -> str:
     """Given path, traverse num directories up from it"""
@@ -34,82 +55,69 @@ def cd_up(path: str, num: int) -> str:
         t_path = os.path.dirname(t_path)
     return t_path
 
+
 def read_yaml(yaml_file):
-    with open(yaml_file, 'r') as stream:
+    with open(yaml_file, "r") as stream:
         try:
             return yaml.safe_load(stream)
         except yaml.YAMLError as exc:
             print(exc)
 
+
 def list_to_coll(list_in: list, out_file: str):
-    '''
+    """
     Turns a list into a coll.
 
     list: Provide a list to convert
     out_file: a path to a file where the coll will be saved
-    '''
-    f = open(out_file, 'w+')
+    """
+    f = open(out_file, "w+")
     counter = 0
     for item in list_in:
-        f.write(f'{counter}, {item};')
+        f.write(f"{counter}, {item};")
         counter += 1
     f.close()
 
-def check_size(path: str, min_size: int) -> bool:
-    """
-    Check's the size of a fyle in bytes.
-    Returns true if the file has a size.
-    Used to avoid empty files.
-    """
-    try:
-        if os.path.getsize(path) >= min_size and os.path.getsize(path) <= 150000000:
-            return True
-    except OSError:
-        return False
 
-def check_ext(path: str, extensions: list) -> bool:
-    """Given a path and a list of legal extensions it either returns false or true."""
-    ext = os.path.splitext(path)[1]
-    try:
-        dummy = extensions.index(ext)
-    except ValueError:
-        return True
-    else:
-        return False
-    
 def wipe_dir(dir: str):
     """Wipe a directory given a path"""
     for file_name in os.listdir(dir):
         os.remove(os.path.join(dir, file_name))
 
+
 def bytes_to_mb(val: int) -> float:
     """convert bytes to mb"""
     return val * 0.000001
+
 
 def get_path() -> str:
     """returns path of script being run"""
     return os.path.dirname(os.path.realpath(__file__))
 
+
 # TODO Fix the parameter inputs naming (I think its backwards with ms2smaps)
 def samps2ms(ms: float, sr: int) -> float:
-    '''
+    """
     convert samples to milliseconds given a sampling rate
-    '''
+    """
     return (ms / sr) * 1000.0
 
+
 def ms2samps(samples: int, sr: int) -> int:
-    '''
+    """
     convert milliseconds to samples given a sample rate
-    '''
-    return (samples/1000) * sr
+    """
+    return (samples / 1000) * sr
+
 
 def ds_store(file_list: list) -> list:
-    '''
+    """
     Remove .DS_Store if in a list
-    '''
-    if '.DS_Store' in file_list:
-        file_list.remove('.DS_Store')
+    """
+    if ".DS_Store" in file_list:
+        file_list.remove(".DS_Store")
     return file_list
+
 
 def bufspill(audio_file_path: str):
     """Reads an audio file and converts its content to a numpy array"""
@@ -117,18 +125,21 @@ def bufspill(audio_file_path: str):
         t_data, _ = sf.read(audio_file_path)
         return t_data.transpose()
     except:
-        print(f'Could not read: {audio_file_path}')
+        print(f"Could not read: {audio_file_path}")
+
 
 def write_json(json_file_path: str, in_dict: dict):
     """Takes a dictionary and writes it to JSON file"""
-    with open(json_file_path, 'w+') as fp:
-        rj.dump(in_dict, fp, indent=4) 
+    with open(json_file_path, "w+") as fp:
+        rj.dump(in_dict, fp, indent=4)
+
 
 def read_json(json_file_path: str) -> dict:
     """Takes a JSON file and returns a dictionary"""
-    with open(json_file_path, 'r') as fp:
+    with open(json_file_path, "r") as fp:
         data = rj.load(fp)
         return data
+
 
 def walkman(audio_path: str):
     """Play a sound file given a path to a valid piece of audio"""
@@ -136,10 +147,12 @@ def walkman(audio_path: str):
     play_obj = wave_obj.play()
     play_obj.wait_done()
 
+
 def printp(string_to_print: str):
     """A uniform way for printing status updates in scripts"""
-    print(f'\n---- {string_to_print} ----')
+    print(f"\n---- {string_to_print} ----\n")
+
 
 def printe(string_to_print: str):
     """A uniform way for printing errors in scripts"""
-    print(f'!!!! {string_to_print} !!!!')
+    print(f"\n!!!! {string_to_print} !!!!")
