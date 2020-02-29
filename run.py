8832b467a3acf199c398eb9bd07b4c14e7e71e83
James Bradbury <jamesbradbury93@gmail.com>
2020-02-29 22:39:19 +0000

    run inside __main__ for mulltithreading compat

--- run.py
+++ run.py
@@ -1,16 +1,13 @@
 import argparse
-from ftis.common.utils import read_yaml
 from ftis.process import FTISProcess
 
-parser = argparse.ArgumentParser(description='The entry point to ftis.')
-parser.add_argument('-c', '--config', type=str, required=True, help='A YAML configurations')
+parser = argparse.ArgumentParser(description="The entry point to ftis.")
+parser.add_argument(
+    "-c", "--config", type=str, required=True, help="A YAML configurations"
+)
 args = parser.parse_args()
 
-# Create an instance of the FTISContainer which holds the lifecycle of an ftis process
-process = FTISProcess()
-process.config = read_yaml(args.config) # Load a configuration file
-process.run_process()
-
-# process.process_config() # Parse/process the YAML
-
-# print(process.config)
+if __name__ == "__main__":
+    # Create an instance of the FTISProcess
+    process = FTISProcess(args.config)
+    process.run_process()
