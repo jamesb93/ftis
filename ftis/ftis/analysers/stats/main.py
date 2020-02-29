2d7a4e4ea5fd45fc00bd3f522cac3c5a91a0c21d
James Bradbury <jamesbradbury93@gmail.com>
2020-02-29 22:40:58 +0000

    add stats analyser

--- ftis/ftis/analysers/stats/main.py
+++ ftis/ftis/analysers/stats/main.py
@@ -1,76 +1,69 @@
 import os
-import sys
-import tempfile
-import subprocess
-import multiprocessing as mp
+import math
+import numpy as np
+from scipy import stats
 from ftis.common.analyser import FTISAnalyser
-from ftis.common.utils import bufspill, write_json, read_json, printp
-from ftis.common.exceptions import NotYetImplemented
-from shutil import rmtree
+from ftis.common.utils import printp, write_json, read_json
 
 
 class STATS(FTISAnalyser):
     def __init__(self, config):
         super().__init__(config)
-        self.name = "stats"
-        self.parameters = {"numderivs": 1}
+        self.name = "stats" #TODO: grab this from the parent folder to only have to change one place
         self.validate_parameters()
-        self.output_type = ".json"
-        self.input_type = ".json"
 
-    def validate_io(self):
-        raise NotYetImplemented
+    def set_output(self, base_dir: str):
+        self.output = os.path.join(base_dir, f"{self.name}.json")
+
+    @staticmethod
+    def calc_stats(data: np.array):
+        """
+        Given a time series calculate statistics
+        """
+        describe = stats.describe(data)
+        mean = describe.mean
+        stddev = math.sqrt(describe.variance)
+        skewness = describe.skewness
+        kurtosis = describe.kurtosis
+        minimum = describe.minmax[0]
+        median = np.median(data)
+        maximum = describe.minmax[1]
+        return [mean, stddev, skewness, kurtosis, minimum, median, maximum]
+
+    def get_stats(self, base_data, num_derivs) -> list:
+        """
+        Given stats on n number derivatives from initial data
+        """
+        container = []
+        for i in range(num_derivs):
+            deriv = np.diff(base_data, i + 1)
+            stats = self.calc_stats(deriv)
+            container.append(stats)
+        return container
+
+
 
     def run(self):
-        workables = read_json(self.input)
-        # Recursively grab all the files from the input string
-        # Prepare for work
-        tmp_dir = tempfile.mkdtemp()
+        data = read_json(self.input)
+        stats_dict = {}
+        try:
+            del data["meta"]
+        except KeyError:
+            print("No metadata to delete")
 
-        def analyse(workable: str):
-            # Setup paths/files etc
-            mfcc_src = workable
-            base_name = os.path.basename(workable)
-            mfcc_features = os.path.join(tmp_dir, f"{base_name}_features.wav")
-            # Compute MFCC descriptor
-            subprocess.call(
-                [
-                    "fluid-mfcc",
-                    "-source",
-                    mfcc_src,
-                    "-features",
-                    mfcc_features,
-                    "-fftsettings",
-                    fftsettings[0],
-                    fftsettings[1],
-                    fftsettings[2],
-                    "-numbands",
-                    str(self.parameters["numbands"]),
-                    "-numcoeffs",
-                    str(self.parameters["numcoeffs"]),
-                    "-maxnumcoeffs",
-                    str(self.parameters["numcoeffs"]),
-                ]
-            )
+        for element in data:  # for key (audio file ) in dict
+            element_container = []
+            print(element)
+            for row in data[element]:  # for mfcc band in mfcc
+                row_stats = self.get_stats(row, self.parameters["numderivs"]) # get the stats for each mfcc band
+                element_container.append(row_stats) # 
 
-            data = bufspill(mfcc_features)
-            list_data = data.tolist()
-            mfcc_dict[workable] = list_data
+            if self.parameters["flatten"]:
+                element_container = np.array(element_container) # Convert to numpy array
+                element_container = element_container.flatten()
+                element_container = element_container.tolist()
+            stats_dict[element] = element_container
+        
+        write_json(self.output, stats_dict)
 
-        num_jobs = len(workables)
-
-        # Insert metadata at the top
-        mfcc_dict["meta"] = {
-            "fftsettings": self.parameters["fftsettings"],
-            "numbands": self.parameters["numbands"],
-            "numcoeffs": self.parameters["numcoeffs"],
-            "numjobs": num_jobs,
-        }
-
-        for index, workable in enumerate(workables):
-            analyse(workable)
-            sys.stdout.write(f"\rAnalayse Progress {index/num_jobs}")
-
-        write_json(self.output, dict(mfcc_dict))
-        rmtree(tmp_dir)
-        printp("Finished MFCC analysis")
+        printp("Finished getting stats")
