1eb82812f04e79b7922b43ee2086e3cb8db4e4cf
James Bradbury <jamesbradbury93@gmail.com>
2020-02-29 22:41:22 +0000

    cleanup analyser class

--- ftis/ftis/common/analyser.py
+++ ftis/ftis/common/analyser.py
@@ -1,28 +1,72 @@
-from ftis.common.exceptions import AnalyserParameterInvalid
+import os
+from ftis.common.exceptions import AnalyserParameterInvalid, NotYetImplemented
+from ftis.common.utils import read_yaml
 
-class FTISAnalyser():
+#TODO: types for input and output
+#TODO: required parameters and optional
+#TODO: stereo > mono analyser
+class FTISAnalyser:
     """
     Every analyser inherits from this class
     """
+
     def __init__(self, config):
-        self.analyser_config = config
-        self.parameters = {} # A place to store the parameters associated to this analyser
+        self.config = config
+        self.input = ""
+        self.output = ""
+        self.parameters = {}
+        self.parameter_template = {}
+        self.name = ""
         self.input_type = ""
         self.output_type = ""
-        self.name = ""
-        
+
     def validate_parameters(self):
-        print('Validating parameters')
-        for key in self.analyser_config.keys():
-            if key not in self.parameters.keys():
-                raise AnalyserParameterInvalid(f"'{key}' is not a parameter for {self.name}. Please review your YAML configuration")
-    
-    def assign_parameters(self):
-        print('Assigning Parameters')
-    
-    @classmethod
-    def run(cls, input, output):
+        """
+        Validates parameters set in the process against the template
+        """
+        #
+        module_parameters = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            "..",
+            "analysers",
+            self.name,
+            "parameters.yaml",
+        )
+        self.parameter_template = read_yaml(module_parameters)
+        self.parameters = self.config["analysers"][self.name]
+
+        # Check that any defined parameters actually exist
+        for key in self.parameters:
+            if key not in self.parameter_template:
+                raise AnalyserParameterInvalid(f"{key} does not exist for {self.name}")
+
+        # If some required keys aren't defined then use defaults
+        for key in self.parameter_template:
+            if key not in self.parameters:
+                self.parameters[key] = self.parameter_template[key]["default"]
+
+        # TODO: Ensure any keys that have options
+
+    def metadata(self):
+        """
+        TODO: Replace the hardcoded metadata creation
+        """
+
+        raise NotYetImplemented
+
+    def set_output(self, base_dir: str):
+        """
+        Method to create the right output for each analyser
+        """
+
+    def validate_io(self):
+        """
+        I validate whether the input and output types are correct.
+        I also create the input and output strings for the class.
+        This needs to be implemented in the module definition.
+        """
+
+    def run(self):
         """
         Method for running the processing chain from input to output.
         """
-        
