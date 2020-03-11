aa8e05d4b47b8367c087110adf829d3079b3f029
James Bradbury <jamesbradbury93@gmail.com>
2020-03-11 17:16:37 +0000

    templates for analyser generation

---
+++ ftis/ftis/templates.py
@@ -0,0 +1,52 @@
+init = '''
+from .main import @NAME@ as main
+
+__all__ = ["main"]
+'''
+main = '''
+from ftis.common.analyser import FTISAnalyser
+from ftis.common.utils import printp
+
+
+class @NAME@(FTISAnalyser):
+    def __init__(self, parent_process):
+        """
+        This is the bare minimum required to instantiate the object.
+        """
+        super().__init__(parent_process)
+        self.logger.debug("Creating @NAME@ instance")
+        self.name = "@NAME@"
+        self.validate_parameters()
+
+    def set_output(self, base_dir: str):
+        """
+        In this method you provide the logic for setting the output.
+        An example here is given for outputting a json file.
+        Any output is valid providing the next link in the chain accepts it.
+        """
+        self.output = os.path.join(base_dir, f"{self.name}.json")
+        self.logger.debug("Setting output for @NAME@")
+
+
+    def run(self):
+        """
+        In this method you implement the functionality for the analyser
+        """
+        self.logger.info("Starting @NAME@")
+        self.logger.info("Finished @NAME@")
+
+'''
+
+parameters = '''
+parameter1:
+    default: "4096 1024 4096"
+    type: string
+
+parameter2:
+    default: 40
+    type: int
+
+parameter3:
+    default: 13.32
+    type: float
+'''
