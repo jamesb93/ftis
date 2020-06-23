f0cb042c1ddb5a586ddca40480724659527348c2
James Bradbury <jamesbradbury93@gmail.com>
2020-06-23 23:18:28 +0100

    simplify imports

--- ftis/ftis/analysers/fluid_loudness/main.py
+++ ftis/ftis/analysers/fluid_loudness/main.py
@@ -1,13 +1,7 @@
 
-import os
-import sys
-import shutil
 import flucoma
-import tempfile
-import subprocess
-import multiprocessing
+from multiprocessing import Manager
 from flucoma import fluid
-from shutil import rmtree
 from ftis.common.analyser import FTISAnalyser
 from ftis.common.exceptions import BinError
 from ftis.common.utils import bufspill, write_json, get_workables, filter_extensions
@@ -21,7 +15,7 @@ class FLUID_LOUDNESS(FTISAnalyser):
         self.name = "FLUID_LOUDNESS".lower()
         self.input_type = Ftypes.folder
         self.output_type = Ftypes.json
-        self.data_container = multiprocessing.Manager().dict()
+        self.data_container = Manager().dict()
 
     def analyse(self, workable: str, task, progress_bar):
 
