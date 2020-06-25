360cc96a6cd2a6fd80b3bae9a52bc62bb216fb9d
James Bradbury <jamesbradbury93@gmail.com>
2020-06-25 20:34:08 +0100

    remove package specific deps for now

--- ftis/setup.py
+++ ftis/setup.py
@@ -6,18 +6,5 @@ setuptools.setup(
     author="James Bradbury",
     author_email="jamesbradbury93@gmail.com",
     description="The finding things in stuff package.",
-    packages=["ftis"],
-    install_requires=[
-        "PyYAML",
-        "python-rapidjson",
-        "SoundFile",
-        "simpleaudio",
-        "sklearn",
-        "hdbscan",
-        "numpy",
-        "scikit-learn",
-        "scipy",
-        "umap-learn",
-        "pydub",
-    ],
+    packages=["ftis"]
 )
