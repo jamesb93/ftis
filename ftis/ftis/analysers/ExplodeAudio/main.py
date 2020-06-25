26ad310779f78898ca25168673668222839fd9aa
James Bradbury <jamesbradbury93@gmail.com>
2020-06-25 20:54:16 +0100

    use pathlib for directory checking

--- ftis/ftis/analysers/ExplodeAudio/main.py
+++ ftis/ftis/analysers/ExplodeAudio/main.py
@@ -1,12 +1,11 @@
 
-import os
 import subprocess
 import numpy as np
 from pydub import AudioSegment
 from pydub.utils import mediainfo
 from ftis.common.analyser import FTISAnalyser
 from ftis.common.types import Ftypes
-from ftis.common.utils import samps2ms, bufspill, check_make, read_json
+from ftis.common.utils import samps2ms, bufspill, read_json
 from ftis.common.proc import multiproc
 
 
@@ -18,22 +17,28 @@ class ExplodeAudio(FTISAnalyser):
         self.name = "ExplodeAudio"
     
     def segment(self, filename:str, task, progress_bar):
-        basic_name = os.path.splitext(os.path.basename(filename))[0]
-        seg_folder = os.path.join(self.output, basic_name)
-        check_make(seg_folder)
+        """Receives a full path to an audio file"""
+        basic_name = filename.name
+        seg_folder = self.output / basic_name
+        if not seg_folder.exists():
+            seg_folder.mkdir()
         slices = self.slice_data[filename] 
         src = AudioSegment.from_file(filename, format="wav")
-        sr = mediainfo(filename)["sample_rate"]
-        for i in range(len(slices) - 1):
-            start = samps2ms(slices[i], int(sr))
-            end = samps2ms(slices[i+1], int(sr))
+        sr = int(mediainfo(filename)["sample_rate"])
+        
+        for i, (start, end) in enumerate(zip(slices, slices[1:])):
+            start = samps2ms(start, sr)
+            end = samps2ms(end, sr)
             segment = src[start:end]
+            
             segment.export(
-                os.path.join(seg_folder, f"{i}_{basic_name}.wav"), 
-                format="wav")
+                seg_folder / f"{i}_{basic_name}.wav",
+                format="wav"
+            )
+
         progress_bar.update(task, advance=1)
 
     def run(self):
         self.slice_data = read_json(self.input)
-        workables = [x for x in self.slice_data.keys()]
+        workables = [Path(x) for x in self.slice_data.keys()]
         multiproc(self.name, self.segment, workables)
