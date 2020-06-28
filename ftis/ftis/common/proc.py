327776d1806e3e31a4e0113355edf6abd2be7477
James Bradbury <jamesbradbury93@gmail.com>
2020-06-28 22:06:48 +0100

    implement a single threaded UI wrapper

--- ftis/ftis/common/proc.py
+++ ftis/ftis/common/proc.py
@@ -8,8 +8,15 @@ def multiproc(name: str, process, workables):
         with ThreadPoolExecutor() as pool:
             futures = [pool.submit(process, work) for work in workables]
             for result in as_completed(futures):
-                progress.update(task, advance=1)
+                progress.advnace(task)
 
+def singleproc(name: str, process, workables):
+    """This function wraps up a multithreaded worker and progress bar"""
+    with Progress() as progress:
+        task = progress.add_task(name, total=len(workables))
+        for x in workables:
+            process(x)
+            progress.advance(task)
 
 def staticproc(name: str, process):
     """For processes where progress can not be determined"""
