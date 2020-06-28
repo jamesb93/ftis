9073909ff99e9bd6b6be5b01f37a354dc8808c0f
James Bradbury <jamesbradbury93@gmail.com>
2020-06-28 22:06:36 +0100

    use singleproc for this kind of process

--- ftis/ftis/analysers/ClusteredNMF/main.py
+++ ftis/ftis/analysers/ClusteredNMF/main.py
@@ -1,7 +1,7 @@
 import numpy as np
 from ftis.common.analyser import FTISAnalyser
 from ftis.common.utils import read_json, write_json
-from ftis.common.proc import multiproc
+from ftis.common.proc import singleproc
 from ftis.common.types import Ftypes
 from sklearn.cluster import AgglomerativeClustering
 from flucoma import fluid
@@ -15,7 +15,6 @@ class ClusteredNMF(FTISAnalyser):
         self.output_type = Ftypes.folder
 
     def analyse(self, workable):
-        print('doing nmf')
         nmf = fluid.nmf(
             workable, 
             iterations=self.parameters["iterations"], 
@@ -24,7 +23,6 @@ class ClusteredNMF(FTISAnalyser):
         bases = get_buffer(nmf.bases, "numpy")
         bases_smoothed = np.zeros_like(bases)
         
-        print('Smoothing bases')
         for i, x in enumerate(bases):
             bases_smoothed[i] = savgol_filter(x, 
                 self.parameters["smoothing"], 
@@ -34,13 +32,11 @@ class ClusteredNMF(FTISAnalyser):
             min_cluster_size=self.parameters["clustersize"], 
             min_samples=self.parameters["samples"])
 
-        print('Clustering')
         cluster_labels = clusterer.fit_predict(bases)
         unique_clusters = list(dict.fromkeys(cluster_labels))
 
         sound = get_buffer(resynth, "numpy")
 
-        print('Amalgamating')
         for x in unique_clusters:
             summed = np.zeros_like(sound[0]) #make an empty numpy array of same size
             base = workable.name
@@ -52,6 +48,4 @@ class ClusteredNMF(FTISAnalyser):
 
     def run(self):
         workables = [str(k) for k in self.input.iterdir() if k.name != ".DS_Store"]
-        # multiproc(self.name, self.analyse, workables)
-        for x in workables:
-            self.analyse(x)
+        singleproc(self.name, self.analyse, workables)
