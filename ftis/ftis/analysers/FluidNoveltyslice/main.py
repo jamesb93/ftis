64c9f3d580a2bf374d9cd29d2e578995a4ff5e4d
James Bradbury <jamesbradbury93@gmail.com>
2020-06-28 23:04:03 +0100

    simplify noveltyslice

--- ftis/ftis/analysers/FluidNoveltyslice/main.py
+++ ftis/ftis/analysers/FluidNoveltyslice/main.py
@@ -21,20 +21,16 @@ class FluidNoveltyslice(FTISAnalyser):
         self.output_type = Ftypes.json
         self.data_container = Manager().dict()
         
-    def analyse(self, workable:str):
-        src = workable
-        noveltyslice = fluid.noveltyslice(
-            src,
+    def analyse(self, workable):
+        noveltyslice = fluid.noveltyslice(workable
             feature = self.parameters["feature"],
             fftsettings = self.parameters["fftsettings"],
             filtersize = self.parameters["filtersize"],
             minslicelength = self.parameters["minslicelength"],
-            threshold = self.parameters["threshold"]
-        )
+            threshold = self.parameters["threshold"])
         self.data_container[str(workable)] = flucoma.utils.get_buffer(noveltyslice)
 
     def run(self):
-        workables = get_workables(self.input)
-        workables = filter_extensions(workables, [".wav"])
+        workables = [x for x in self.input.iterdir() if x.suffix == ".wav"]
         multiproc(self.name, self.analyse, workables)
         write_json(self.output, dict(self.data_container))
