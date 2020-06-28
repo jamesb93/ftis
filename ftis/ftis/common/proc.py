08a4dca6ff2786d2041b90ae4f6eb582aec0f91a
James Bradbury <jamesbradbury93@gmail.com>
2020-06-28 22:34:34 +0100

    go back to update func

--- ftis/ftis/common/proc.py
+++ ftis/ftis/common/proc.py
@@ -8,7 +8,8 @@ def multiproc(name: str, process, workables):
         with ThreadPoolExecutor() as pool:
             futures = [pool.submit(process, work) for work in workables]
             for result in as_completed(futures):
-                progress.advnace(task)
+                progress.update(task, advance=1)
+                
 
 def singleproc(name: str, process, workables):
     """This function wraps up a multithreaded worker and progress bar"""
@@ -16,7 +17,7 @@ def singleproc(name: str, process, workables):
         task = progress.add_task(name, total=len(workables))
         for x in workables:
             process(x)
-            progress.advance(task)
+            progress.update(task, advance=1)
 
 def staticproc(name: str, process):
     """For processes where progress can not be determined"""
