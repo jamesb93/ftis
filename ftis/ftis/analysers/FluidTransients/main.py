fa621d73c72dab03e212711e25a97bbd2fd9d4a2
James Bradbury <jamesbradbury93@gmail.com>
2020-06-29 12:17:32 +0100

    fix transient extractor

--- ftis/ftis/analysers/FluidTransients/main.py
+++ ftis/ftis/analysers/FluidTransients/main.py
@@ -1,14 +1,8 @@
-
-import os
-import sys
-import multiprocessing
-import subprocess
-import shutil
+from flucoma import fluid
+from flucoma.utils import cleanup
 from ftis.common.analyser import FTISAnalyser
 from ftis.common.proc import multiproc
 from ftis.common.types import Ftypes
-from ftis.common.exceptions import BinError
-from ftis.common.utils import get_workables
 
 
 class FluidTransients(FTISAnalyser):
@@ -17,33 +11,28 @@ class FluidTransients(FTISAnalyser):
         self.input_type = Ftypes.folder
         self.output_type = Ftypes.folder
 
-    def analyse(self, workable: str, task, progress_bar):
-        # Setup paths/files etc
-        src = workable
-        base_name = os.path.basename(workable)
-        residual = os.path.join(self.output, f"{base_name}_residual.wav")
-        transients = os.path.join(self.output, f"{base_name}_transients.wav")
-        subprocess.call([
-                "fluid-transients",
-                "-source", src,
-                "-transients", transients,
-                "-residual", residual,
-                "-blocksize", str(self.parameters["blocksize"]),
-                "-clumplength", str(self.parameters["clumplength"]),
-                "-order", str(self.parameters["order"]),
-                "-padsize", str(self.parameters["padsize"]),
-                "-skew", str(self.parameters["skew"]),
-                "-threshback", str(self.parameters["threshback"]),
-                "-threshfwd", str(self.parameters["threshfwd"]),
-                "-windowsize", str(self.parameters["windowsize"])
-        ])
-        progress_bar.update(task, advance = 1)
+    def analyse(self, workable):
+        out_folder = self.output / workable.name
+        out_folder.mkdir(exist_ok=True)
 
+        transients = out_folder / f"transients_{workable.name}"
+        residual = out_folder / f"residual_{workable.name}"
+
+        fluid.transients(workable,
+            transients=transients,
+            residual=residual,
+            blocksize = self.parameters["blocksize"],
+            clumplength = self.parameters["clumplength"],
+            order = self.parameters["order"],
+            padsize = self.parameters["padsize"],
+            skew = self.parameters["skew"],
+            threshback = self.parameters["threshback"],
+            threshfwd = self.parameters["threshfwd"],
+            windowsize = self.parameters["windowsize"]
+        )
 
     def run(self):
-        """
-        In this method you implement the functionality for the analyser.
-        Ideally, place the logic between the two loggers.
-        """
-        workables = get_workables(self.input, ('.wav'))
+        workables = [x for x in self.input.iterdir() if x.suffix == ".wav"]
         multiproc(self.name, self.analyse, workables)
+        cleanup()
+
