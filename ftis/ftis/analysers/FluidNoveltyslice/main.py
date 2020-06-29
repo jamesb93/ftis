673c9063c6acc7c876206ef57e8aa1bea0bf5156
James Bradbury <jamesbradbury93@gmail.com>
2020-06-29 12:18:54 +0100

    refactor noveltyslice

--- ftis/ftis/analysers/FluidNoveltyslice/main.py
+++ ftis/ftis/analysers/FluidNoveltyslice/main.py
@@ -1,12 +1,6 @@
 
-import os
-
-import flucoma
-import tempfile
-import subprocess
-import numpy as np
-from shutil import rmtree
 from flucoma import fluid
+from flucoma.utils import get_buffer, cleanup
 from multiprocessing import Manager
 from ftis.common.analyser import FTISAnalyser
 from ftis.common.types import Ftypes
@@ -22,15 +16,18 @@ class FluidNoveltyslice(FTISAnalyser):
         self.data_container = Manager().dict()
         
     def analyse(self, workable):
-        noveltyslice = fluid.noveltyslice(workable
+        noveltyslice = fluid.noveltyslice(
+            workable,
             feature = self.parameters["feature"],
             fftsettings = self.parameters["fftsettings"],
             filtersize = self.parameters["filtersize"],
             minslicelength = self.parameters["minslicelength"],
-            threshold = self.parameters["threshold"])
-        self.data_container[str(workable)] = flucoma.utils.get_buffer(noveltyslice)
+            threshold = self.parameters["threshold"]
+        )
+        self.data_container[str(workable)] = get_buffer(noveltyslice)
 
     def run(self):
         workables = [x for x in self.input.iterdir() if x.suffix == ".wav"]
         multiproc(self.name, self.analyse, workables)
         write_json(self.output, dict(self.data_container))
+        cleanup()
