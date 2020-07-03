c9860d7de7357de4300471ef288fb22699e654bc
James Bradbury <jamesbradbury93@gmail.com>
2020-07-03 18:52:23 +0100

    conversion functions in new granular file

---
+++ ftis/ftis/common/conversion.py
@@ -0,0 +1,8 @@
+def samps2ms(samples: float, sr: int) -> float:
+    """samples to milliseconds given a sampling rate"""
+    return (samples / sr) * 1000.0
+
+
+def ms2samps(ms: int, sr: int) -> int:
+    """milliseconds to samples given a sample rate"""
+    return (ms * 0.001) * sr
