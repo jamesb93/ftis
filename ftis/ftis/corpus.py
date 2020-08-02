076d6e1bcd711bdfa8d16a0837f20a0bb77a80c3
James Bradbury <jamesbradbury93@gmail.com>
2020-08-02 14:46:06 +0100

    add caching of the corpus

--- ftis/ftis/corpus.py
+++ ftis/ftis/corpus.py
@@ -2,6 +2,7 @@ from ftis.common.analyser import FTISAnalyser
 from ftis.common.io import get_duration
 from pathlib import Path
 from pydub.utils import mediainfo
+import pickle
 # The corpusloader will be a special kind of analyser used for preparing sound file batches
 
 class CorpusLoader(FTISAnalyser):
@@ -21,6 +22,15 @@ class CorpusLoader(FTISAnalyser):
     def get_items(self):
         self.output = [x for x in self.input.iterdir()]
 
+    #TODO maybe make this a csv so its readable.
+    def load_cache(self):
+        # self.output = read_json(self.dump_path)
+        with open(self.dump_path, "rb") as f:
+            self.output = pickle.load(f)
+
+    def dump(self):
+        with open(self.dump_path, "wb") as f:
+            pickle.dump(self.output, f)
     def filter_items(self):
         # Filter by the extension
         self.output = [x for x in self.output if x.suffix in self.file_type]
