9a9b13961598ff2d6af989147538759332d608b3
James Bradbury <jamesbradbury93@gmail.com>
2020-08-03 22:57:08 +0100

    remove yaml functions

--- ftis/ftis/common/io.py
+++ ftis/ftis/common/io.py
@@ -1,4 +1,3 @@
-import yaml
 import soundfile as sf
 import rapidjson as rj
 
@@ -8,21 +7,11 @@ def write_json(json_file_path: str, in_dict: dict):
     with open(json_file_path, "w+") as fp:
         rj.dump(in_dict, fp, indent=4)
 
-
 def read_json(json_file_path: str) -> dict:
     """Takes a JSON file and returns a dictionary"""
     with open(json_file_path, "r") as fp:
         data = rj.load(fp)
         return data
-
-
-def read_yaml(yaml_file):
-    with open(yaml_file, "r") as stream:
-        try:
-            return yaml.safe_load(stream)
-        except yaml.YAMLError as exc:
-            print(exc)
-
 
 def peek(audio_file_path: str, output: str = "np"):
     """
@@ -35,9 +24,6 @@ def peek(audio_file_path: str, output: str = "np"):
         return data.tolist(), sr
     if output == "np":
         return data, sr
-    
-# def get_duration(path):
-#     return float(mediainfo(path)["duration"])
 
 def get_duration(path):
     data, sr = peek(path)
