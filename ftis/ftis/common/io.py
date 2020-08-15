8c24cb524a3579e37c59361a34d47e25fb7592c6
James Bradbury <jamesbradbury93@gmail.com>
2020-08-15 14:22:03 +0100

    use simdjson for json everything

--- ftis/ftis/common/io.py
+++ ftis/ftis/common/io.py
@@ -1,16 +1,16 @@
 import soundfile as sf
-import rapidjson as rj
+import simdjson
 
 
 def write_json(json_file_path: str, in_dict: dict):
     """Takes a dictionary and writes it to JSON file"""
     with open(json_file_path, "w+") as fp:
-        rj.dump(in_dict, fp, indent=4)
+        simdjson.dump(in_dict, fp, indent=4)
 
 def read_json(json_file_path: str) -> dict:
     """Takes a JSON file and returns a dictionary"""
     with open(json_file_path, "r") as fp:
-        data = rj.load(fp)
+        data = simdjson.load(fp)
         return data
 
 def peek(audio_file_path: str, output: str = "np"):
