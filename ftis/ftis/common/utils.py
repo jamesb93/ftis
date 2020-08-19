c3554f1c4d6093dface395683b833694944159fd
James Bradbury <jamesbradbury93@gmail.com>
2020-08-19 11:49:29 +0100

    make digest a bit smaller

--- ftis/ftis/common/utils.py
+++ ftis/ftis/common/utils.py
@@ -4,15 +4,6 @@ import numpy as np
 from pathlib import Path
 
 
-def get_workables(pth):
-    temp_workables = []
-    for root, _, files in os.walk(pth):
-        for f in files:
-            full_path = Path(root) / f
-            temp_workables.append(full_path)
-    return temp_workables
-
-
 def list_to_coll(list_input: list, out_file: str):
     """Turns a list into a coll."""
     f = open(out_file, "w+")
@@ -27,10 +18,10 @@ def bytes_to_mb(val: int) -> float:
     """convert bytes to mb"""
     return val * 0.000001
 
+
 def create_hash(*items) -> str:
     """Create a hash from a list of items"""
-    m = hashlib.blake2b(digest_size=32)
+    m = hashlib.blake2b(digest_size=20)
     for item in items:
         m.update(str(item).encode('utf-8'))
     return m.hexdigest()
-
