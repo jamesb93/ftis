97e935669b2ec2b0b162a8245744d336772c9d3c
James Bradbury <jamesbradbury93@gmail.com>
2020-08-20 11:42:34 +0100

    simplification and caching

--- ftis/ftis/filters.py
+++ ftis/ftis/filters.py
@@ -18,7 +18,6 @@ class Duration(FTISAnalyser):
         self.output = []
         self.min_dur:int = min_dur
         self.max_dur:int = max_dur
-        self.file_type = file_type
         self.dump_type = ".json"
 
     def load_cache(self):
@@ -34,13 +33,10 @@ class Duration(FTISAnalyser):
         return dur < self.max_dur and dur > self.min_dur
 
     def filter_items(self):
-        self.output = [x for x in self.output if self.filter_duration(x)]
-    
-    def create_corpus(self):
-        self.filter_items()
+        self.output = [x for x in self.input if self.filter_duration(x)]
 
     def run(self):
-        staticproc(self.name, self.create_corpus)
+        staticproc(self.name, self.filter_items)
 
 class Loudness(FTISAnalyser):
     """A way to filter corpus items after they have been loaded by the corpus loader"""
@@ -63,7 +59,7 @@ class Loudness(FTISAnalyser):
         write_json(self.dump_path, d)
 
     def analyse_items(self):
-        self.median_loudness = {}
+        median_loudness = {}
         for x in self.input:
             hsh = create_hash(x, self.min_loudness, self.max_loudness)
             cache = self.process.cache / f"{hsh}.npy"
@@ -73,21 +69,13 @@ class Loudness(FTISAnalyser):
                     "numpy")
                 np.save(cache, med_loudness)
             else:
-                med_loudness = np.load(cache)
+                med_loudness = np.load(cache, allow_pickle=True)
 
-            self.median_loudness[str(x)] = med_loudness[0][5]
-                
-
-    def filter_items(self):
-        # get the required percentile
-        vals = np.array([x for x in self.median_loudness.values()])
-        self.min_perc = np.percentile(vals, self.min_loudness)
-        self.max_perc = np.percentile(vals, self.max_loudness)
-        self.output = [k for k, v in self.median_loudness.items() if v <= self.max_perc and v >= self.min_perc]
-
-    def filter_corpus(self):
-        self.analyse_items()
-        self.filter_items()
+            median_loudness[str(x)] = med_loudness[0][5]
+        vals = np.array([x for x in median_loudness.values()])
+        min_perc = np.percentile(vals, self.min_loudness)
+        max_perc = np.percentile(vals, self.max_loudness)
+        self.output = [k for k, v in median_loudness.items() if v <= max_perc and v >= min_perc]
 
     def run(self):
-        staticproc(self.name, self.filter_corpus)
+        staticproc(self.name, self.analyse_items)
