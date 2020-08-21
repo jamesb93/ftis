fa1932c6de0263e2de79df3200d19bd3832b0bb2
James Bradbury <jamesbradbury93@gmail.com>
2020-08-21 16:06:21 +0100

    move to a granular import structure

---
+++ ftis/ftis/analyser/audio.py
@@ -0,0 +1,70 @@
+from ftis.common.analyser import FTISAnalyser
+from ftis.common.io import write_json, read_json, peek
+from ftis.common.proc import singleproc
+from ftis.common.conversion import samps2ms
+from shutil import copyfile
+from pydub import AudioSegment
+from pydub.utils import mediainfo
+from pathlib import Path
+from scipy.io import wavfile
+
+
+class CollapseAudio(FTISAnalyser):
+    def __init__(self):
+        super().__init__()
+
+    def collapse(self, workable):
+        out = self.outfolder / workable.name
+        raw, sr = peek(workable)
+        audio = None
+        if raw.ndim == 1:
+            audio = raw
+        else:
+            audio = raw.sum(axis=0) / raw.ndim
+        wavfile.write(out, sr, audio)
+
+    def run(self):
+        self.outfolder = self.process.folder / f"{self.order}_{self.__class__.__name__}"
+        self.outfolder.mkdir(exist_ok=True)
+        self.output = [x for x in self.outfolder.iterdir() if x.suffix == ".wav"]
+        singleproc(self.name, self.collapse, self.input)
+
+
+class ExplodeAudio(FTISAnalyser):
+    def __init__(self, cache=False):
+        super().__init__(cache=cache)
+        self.dump_type = ".json"
+
+    def segment(self, workable):
+        # FIXME why do i need to mport here for it to not complain
+        # FIXME Can maybe just move to the top if not a slow import
+
+        self.output_folder = self.process.folder / f"{self.order}_{self.__class__.__name__}"
+        self.output_folder.mkdir(exist_ok=True)
+
+        slices = self.input[str(workable)]
+
+        if len(slices) == 1:
+            copyfile(workable, self.output_folder / f"{workable.stem}_0.wav")
+
+        src = AudioSegment.from_file(workable, format="wav")
+        sr = int(mediainfo(workable)["sample_rate"])
+
+        for i, (start, end) in enumerate(zip(slices, slices[1:])):
+            start = samps2ms(start, sr)
+            end = samps2ms(end, sr)
+            segment = src[start:end]
+            segment.export(self.output_folder / f"{workable.stem}_{i}.wav", format="wav")
+
+    def load_cache(self):
+        d = read_json(self.dump_path)
+        self.output = [Path(x) for x in d["corpus_items"]]
+
+    def dump(self):
+        d = {"corpus_items" : [str(x) for x in self.output]}
+        write_json(self.dump_path, d)
+
+    def run(self):
+        workables = [Path(x) for x in self.input.keys()]
+        singleproc(self.name, self.segment, workables)
+        self.output = [x for x in self.output_folder.iterdir() if x.suffix in ['.wav', '.aiff', '.aif']]
---
+++ ftis/ftis/analyser/descriptor.py
@@ -0,0 +1,258 @@
+from ftis.common.analyser import FTISAnalyser
+from ftis.common.proc import staticproc, multiproc, singleproc
+from ftis.common.io import write_json, read_json
+from ftis.common.utils import create_hash
+from untwist import transforms, data
+from flucoma.utils import get_buffer
+from flucoma import fluid
+from multiprocessing import Manager
+import numpy as np
+import librosa
+
+
+class Flux(FTISAnalyser):
+    """Computes spectral flux of an audio file"""
+
+    def __init__(self, windowsize=1024, hopsize=512, cache=False):
+        super().__init__(cache=cache)
+        self.windowsize = windowsize
+        self.hopsize = hopsize
+        self.dump_type = ".json"
+
+    def load_cache(self):
+        self.output = read_json(self.dump_path)
+
+    def dump(self):
+        write_json(self.dump_path, self.output)
+
+    def flux(self, workable):
+        audio = data.Wave.read(str(workable))
+        if audio.is_stereo():
+            audio = np.sum(audio, axis=1)
+
+        fft = transforms.STFT(fft_size=self.windowsize, hop_size=self.hopsize).process(
+            audio
+        )
+
+        self.buffer[str(workable)] = list(
+            np.sum(np.abs(np.diff(np.abs(fft))), axis=0)
+        )  # Flux calculation here
+
+    def run(self):
+        self.buffer = Manager().dict()
+        multiproc(self.name, self.flux, self.input)
+        self.output = dict(self.buffer)
+
+
+class FluidLoudness(FTISAnalyser):
+    def __init__(self, windowsize=17640, hopsize=4410, kweighting=1, truepeak=1, cache=False):
+        super().__init__(cache=cache)
+        self.windowsize = windowsize
+        self.hopsize = hopsize
+        self.kweighting = kweighting
+        self.truepeak = truepeak
+        self.dump_type = ".json"
+
+    def load_cache(self):
+        self.output = read_json(self.dump_path)
+
+    def dump(self):
+        write_json(self.dump_path, self.output)
+
+    def analyse(self, workable):
+        hsh = create_hash(workable, self.identity)
+        cache = self.process.cache / f"{hsh}.npy"
+
+        if not cache.exists():
+            loudness = get_buffer(
+                fluid.loudness(workable,
+                    windowsize=self.windowsize,
+                    hopsize=self.hopsize,
+                    kweighting=self.kweighting,
+                    truepeak=self.truepeak
+                ), "numpy"
+            )
+            np.save(cache, loudness)
+        else:
+            loudness = np.load(cache, allow_pickle=True)
+        self.buffer[str(workable)] = loudness.tolist()
+
+    def run(self):
+        self.buffer = Manager().dict()
+        workables = self.input
+        multiproc(self.name, self.analyse, workables)
+        self.output = dict(self.buffer)
+
+
+class FluidMFCC(FTISAnalyser):
+    def __init__(self,
+        fftsettings=[1024, 512, 1024],
+        numbands=40,
+        numcoeffs=13,
+        minfreq=80,
+        maxfreq=20000,
+        discard=False,
+        cache=False
+    ):
+        super().__init__(cache=cache)
+        self.fftsettings = fftsettings
+        self.numbands = numbands
+        self.numcoeffs = numcoeffs
+        self.minfreq = minfreq
+        self.maxfreq = maxfreq
+        self.discard = discard
+        self.dump_type = ".json"
+
+    def load_cache(self):
+        self.output = read_json(self.dump_path)
+
+    def dump(self):
+        write_json(self.dump_path, self.output)
+
+    def analyse(self, workable):
+        hsh = create_hash(workable, self.identity)
+        cache = self.process.cache / f"{hsh}.npy"
+        if cache.exists():
+            f = np.load(cache, allow_pickle=True)
+        else:
+            f = get_buffer(
+                fluid.mfcc(
+                    workable,
+                    fftsettings=self.fftsettings,
+                    numbands=self.numbands,
+                    numcoeffs=self.numcoeffs,
+                    minfreq=self.minfreq,
+                    maxfreq=self.maxfreq,
+                ), "numpy"
+            )
+            np.save(cache, f)
+        if self.discard:
+            self.buffer[str(workable)] = f.tolist()[1:]
+        else:
+            self.buffer[str(workable)] = f.tolist()
+
+    def run(self):
+        self.buffer = Manager().dict()
+        multiproc(self.name, self.analyse, self.input)
+        self.output = dict(self.buffer)
+
+
+class LibroMFCC(FTISAnalyser):
+    def __init__(self,
+        numbands=40,
+        numcoeffs=20,
+        minfreq=80,
+        maxfreq=20000,
+        window=2048,
+        hop=512,
+        dct=2,
+        discard=False,
+        cache=False
+    ):
+        super().__init__(cache=cache)
+        self.numbands = numbands
+        self.numcoeffs = numcoeffs
+        self.minfreq = minfreq
+        self.maxfreq = maxfreq
+        self.window = window
+        self.hop = hop
+        self.dct = dct
+        self.discard = discard
+        self.dump_type = ".json"
+
+    def load_cache(self):
+        self.output = read_json(self.dump_path)
+
+    def dump(self):
+        write_json(self.dump_path, self.output)
+
+    def analyse(self, workable):
+        hsh = create_hash(workable, self.identity)
+        cache = self.process.cache / f"{hsh}.npy"
+        if cache.exists():
+            feature = np.load(cache, allow_pickle=True)
+            print('loaded cache')
+        else:
+            y, sr = librosa.load(workable, sr=None, mono=True)
+            feature = librosa.feature.mfcc(y=y, sr=sr,
+                n_mfcc=self.numcoeffs,
+                dct_type=self.dct,
+                n_mels=self.numbands,
+                fmax=self.maxfreq,
+                fmin=self.minfreq,
+                hop_length=self.hop,
+                n_fft=self.window,
+            )
+            np.save(cache, feature)
+
+        if self.discard:
+            self.buffer[str(workable)] = feature.tolist()[1:]
+        else:
+            self.buffer[str(workable)] = feature.tolist()
+
+    def run(self):
+        self.buffer = Manager().dict()
+        singleproc(self.name, self.analyse, self.input)
+        self.output = dict(self.buffer)
+
+
+class LibroCQT(FTISAnalyser):
+    def __init__(self,
+        hop_length=512,
+        minfreq=110,
+        n_bins=84,
+        bins_per_octave=12,
+        tuning=0.0,
+        filter_scale=1,
+        norm=1,
+        sparsity=0.01,
+        window='hann',
+        scale=True,
+        pad_mode='reflect',
+        cache=False
+    ):
+        super().__init__(cache=cache)
+        self.hop_length = hop_length
+        self.minfreq = minfreq
+        self.n_bins = n_bins
+        self.bins_per_octave = bins_per_octave
+        self.tuning = tuning
+        self.filter_scale = filter_scale
+        self.norm = norm
+        self.sparsity = sparsity
+        self.window = window
+        self.scale = scale
+        self.pad_mode = pad_mode
+        self.dump_type = ".json"
+
+    def load_cache(self):
+        self.output = read_json(self.dump_path)
+
+    def dump(self):
+        write_json(self.dump_path, self.output)
+
+    def analyse(self, workable):
+        y, sr = librosa.load(workable, sr=None, mono=True)
+        cqt = librosa.cqt(y, sr, 
+            fmin=self.minfreq,
+            n_bins=self.n_bins,
+            bins_per_octave=self.bins_per_octave,
+            tuning=self.tuning,
+            filter_scale=self.filter_scale,
+            norm=self.norm,
+            sparsity=self.sparsity,
+            window=self.window,
+            scale=self.scale,
+            pad_mode=self.pad_mode
+        )
+        self.buffer[str(workable)] = np.abs(cqt).tolist()
+
+    def run(self):
+        self.buffer = Manager().dict()
+        singleproc(self.name, self.analyse, self.input)
+        self.output = dict(self.buffer)
+
+
+
+
+
