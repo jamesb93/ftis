9149ff8328725da64ceb48286982fc8ee15e0638
James Bradbury <jamesbradbury93@gmail.com>
2020-09-19 00:20:10 +0100

    correct path to readme.md

--- ftis/setup.py
+++ ftis/setup.py
@@ -1,7 +1,7 @@
 import setuptools
 from pathlib import Path
 
-readme = Path("../README.md")
+readme = Path("../readme.md")
 
 with open(readme, encoding="utf-8") as f:
     long_description = f.read()
