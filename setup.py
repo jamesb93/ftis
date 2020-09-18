3cfd7e228e0c58fd9a943b308e44aae4af47a1b9
James Bradbury <jamesbradbury93@gmail.com>
2020-09-19 00:38:22 +0100

    simplify opening readme

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from pathlib import Path
 
-with open("readme.md", encoding="utf-8") as f:
+with open("readme.md", 'r') as f:
     long_description = f.read()
 
 setup(
