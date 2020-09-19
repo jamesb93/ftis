8ea3842a86e4f87a70422a269f124755371930b0
James Bradbury <jamesbradbury93@gmail.com>
2020-09-19 11:49:54 +0100

    bump

--- ftis/setup.py
+++ ftis/setup.py
@@ -6,7 +6,7 @@ with open("README.md", 'r') as f:
 
 setup(
     name="ftis",
-    version="1.0.4",
+    version="1.0.5",
     author="James Bradbury",
     url="https://github.com/jamesb93/ftis",
     license="GLPv3+",
