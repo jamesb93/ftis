4f5b45ec53dd219e570da60224a99bda3b601b70
James Bradbury <jamesbradbury93@gmail.com>
2020-09-20 12:11:15 +0100

    robust scaling

--- ftis/ftis/analyser/scaling.py
+++ ftis/ftis/analyser/scaling.py
@@ -1,7 +1,11 @@
 from ftis.common.analyser import FTISAnalyser
 from ftis.common.io import write_json, read_json
 from ftis.common.proc import staticproc
-from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.preprocessing import (
+    MinMaxScaler, 
+    StandardScaler,
+    RobustScaler
+)
 from joblib import dump as jdump
 import numpy as np
 
@@ -36,7 +40,6 @@ class Standardise(FTISAnalyser):
     def __init__(self, cache=False):
         super().__init__(cache=cache)
         self.dump_type = ".json"
-        from sklearn.preprocessing import StandardScaler
 
     def load_cache(self):
         self.output = read_json(self.dump_path)
@@ -47,6 +50,29 @@ class Standardise(FTISAnalyser):
 
     def analyse(self):
         self.model = StandardScaler()
+        self.model.fit(self.features)
+        scaled_data = self.model.transform(self.features)
+        self.output = {k: list(v) for k, v in zip(self.keys, scaled_data)}
+
+    def run(self):
+        self.keys = [x for x in self.input.keys()]
+        self.features = [f for f in self.input.values()]
+        staticproc(self.name, self.analyse)
+
+class Robust(FTISAnalyser):
+    def __init__(self, cache=False):
+        super().__init__(cache=cache)
+        self.dump_type = ".json"
+
+    def load_cache(self):
+        self.output = read_json(self.dump_path)
+
+    def dump(self):
+        jdump(self.model, self.model_dump)
+        write_json(self.dump_path, self.output)
+
+    def analyse(self):
+        self.model = RobustScaler()
         self.model.fit(self.features)
         scaled_data = self.model.transform(self.features)
         self.output = {k: list(v) for k, v in zip(self.keys, scaled_data)}
