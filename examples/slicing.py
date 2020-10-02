c10995fbbc01b314da2e5e55d8b5617278a042b3
James Bradbury <jamesbradbury93@gmail.com>
2020-10-02 13:53:57 +0100

    add input and outputs args

--- examples/slicing.py
+++ examples/slicing.py
@@ -2,9 +2,15 @@ from ftis.analyser.audio import ExplodeAudio, CollapseAudio
 from ftis.analyser.slicing import FluidNoveltyslice
 from ftis.process import FTISProcess
 from ftis.corpus import Corpus
+import argparse
 
-src = Corpus("~/corpus-folder/corpus1")
-out = "~/corpus-folder/slicing"
+parser = argparse.ArgumentParser(description="Process input and output location")
+parser.add_argument('-i', '--input',  default="~/corpus-folder/corpus1", type=str, help="Folder for input. This should contain some audio files.")
+parser.add_argument('-o', '--output', default="~/corpus-folder/slicing", type=str, help='Folder for output. This will be made if it doesnt exist.')
+args = parser.parse_args()
+
+src = Corpus(args.input)
+out = args.output
 
 process = FTISProcess(source=src, folder=out)
 
@@ -13,4 +19,5 @@ process.add(
     ExplodeAudio()
 )
 
-process.run()
+if __name__ == "__main__":
+    process.run()
