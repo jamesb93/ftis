fef1e3dd04386e935d9e866cd793d17b1eca6788
James Bradbury <jamesbradbury93@gmail.com>
2020-10-08 23:48:41 +0100

    whitespace

--- ftis/ftis/analyser/dr.py
+++ ftis/ftis/analyser/dr.py
@@ -3,7 +3,6 @@ from ftis.common.io import write_json, read_json
 from ftis.common.proc import staticproc
 from umap import UMAP as umapdr
 from joblib import dump as jdump
-
 import numpy as np
 
 
@@ -34,7 +33,6 @@ class UMAP(FTISAnalyser):
         self.model = umapdr(n_components=self.components, n_neighbors=self.neighbours, min_dist=self.mindist, random_state=42)
         self.model.fit(data)
         transformed_data = self.model.transform(data)
-
         self.output = {k: v.tolist() for k, v in zip(keys, transformed_data)}
 
     def run(self):
