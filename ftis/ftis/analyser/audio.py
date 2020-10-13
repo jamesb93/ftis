2311cf046fd444e63e22b3e481a78235f3d9e0c1
James Bradbury <jamesbradbury93@gmail.com>
2020-10-13 21:00:23 +0100

    to be removed

--- ftis/ftis/analyser/audio.py
+++ ftis/ftis/analyser/audio.py
@@ -21,7 +21,7 @@ class CollapseAudio(FTISAnalyser):
             audio = raw
         else:
             audio = raw.transpose().sum(axis=0) / raw.ndim
-        wavfile.write(out, sr, audio)
+        # wavfile.write(out, sr, audio)
         sf.write(out, audio, sr, "PCM_32")
 
     def run(self):
