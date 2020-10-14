d92ab9ff5e4818f567222cc157b668987daaeddb
James Bradbury <jamesbradbury93@gmail.com>
2020-10-14 11:24:46 +0100

    new dream interface

--- examples/overloading.py
+++ examples/overloading.py
@@ -3,18 +3,21 @@ from ftis.process import FTISProcess
 from ftis.analyser.audio import CollapseAudio
 from ftis.analyser.descriptor import FluidLoudness, FluidPitch
 
-from pathlib import Path
 
-corpus = Corpus("~/corpus-folder/corpus")
+corpus = Corpus("~/corpus-folder/corpus1")
+f = CollapseAudio() 
+chain1 = corpus >> f >> FluidLoudness()
+chain2 = corpus >> f >> FluidPitch()
 
-chain1 = CollapseAudio() >> FluidLoudness()
-chain2 =  CollapseAudio() >> FluidPitch()
+print(chain1.chain)
 
-world = World(
-    source = corpus
-)
 
-world.add_nodes(
-    chain1,
-    chain2
-)`
+
+# world = World(
+#     source = corpus
+# )
+
+# world.add_nodes(
+#     chain1,
+#     chain2
+# )`
