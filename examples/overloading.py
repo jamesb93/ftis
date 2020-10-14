58a4078ec88d3b94bb8fe59aa14f1d303fe0558c
James Bradbury <jamesbradbury93@gmail.com>
2020-10-14 14:28:43 +0100

    interface update

--- examples/overloading.py
+++ examples/overloading.py
@@ -1,23 +1,23 @@
 from ftis.corpus import Corpus
 from ftis.process import FTISProcess
 from ftis.analyser.audio import CollapseAudio
-from ftis.analyser.descriptor import FluidLoudness, FluidPitch
-
+from ftis.analyser.descriptor import FluidLoudness, FluidPitch, FluidMFCC
+from pathlib import Path
 
 corpus = Corpus("~/corpus-folder/corpus1")
-f = CollapseAudio() 
-chain1 = corpus >> f >> FluidLoudness()
-chain2 = corpus >> f >> FluidPitch()
+f = CollapseAudio()
+corpus >> f >> FluidPitch()
 
-print(chain1.chain)
+def get_children(obj):
+    print(f'***{obj} children***')
+    for x in obj.chain:
+        print(x)
+        get_children(x)
 
-
-
-# world = World(
-#     source = corpus
-# )
-
-# world.add_nodes(
-#     chain1,
-#     chain2
-# )`
+# get_children(corpus)
+# get_children(corpus)
+# chain1 = f >> FluidLoudness()
+# chain2 = f >> FluidPitch()
+# chain3 = f >> FluidMFCC()
+# print(chain1.chain[f])
+# print(chain1.chain)
