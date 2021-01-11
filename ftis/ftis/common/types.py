10815a9b7ee69fa980569ebc49ec27cd25208c04
James Bradbury <jamesbradbury93@gmail.com>
2021-01-11 16:51:46 +0000

    add support for types

--- ftis/ftis/common/types.py
+++ ftis/ftis/common/types.py
@@ -1,16 +1,32 @@
 from dataclasses import dataclass, field
 
+@dataclass
+class FTISType:
+    """
+    Base type for FTIS objects
+    Everything inside is a dictionary
+    """
+    data:dict = field(default_factory=dict)
+
+    def __iter__(self):
+        yield from self.data
+
+    def __len__(self):
+        return len(self.data)
 
 @dataclass
-class Indices:
-    data:dict = field(default_factory=dict)
+class Indices(FTISType):
+    def __iter__(self):
+        yield from self.data.items()
     ext:str = ".json"
 
 @dataclass
-class AudioFiles:
-    data:dict = field(default_factory=dict)
+class AudioFiles(FTISType):
+    ext:str = ".json"
+    def __post_init__(self):
+        """Ensure that the paths are for audio files"""
+        self.data = {x for x in self.data if x.suffix in [".wav", ".aiff", ".aif"]}
 
 @dataclass
-class Data:
-    data:dict = field(default_factory=dict)
+class Data(FTISType):
     ext:str = ".json"
